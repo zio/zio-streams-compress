56841fe941b23b260a21139555e1be946e96c809
Erik van Oosten <e.vanoosten@grons.nl>
2026-08-09 17:43:05 +0200

    Update readme and regenerate site (#246)

---
+++ website/docs/sidebars.js
@@ -0,0 +1,13 @@
+const sidebars = {
+  sidebar: [
+    {
+      type: "category",
+      label: "ZIO Streams Compress",
+      collapsed: false,
+      link: { type: "doc", id: "index" },
+      items: []
+    }
+  ]
+};
+
+module.exports = sidebars;
